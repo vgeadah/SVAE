f4e0a1943f63419020ba438af5377e8234608fe4
Victor Geadah <vg0233@princeton.edu>
2023-01-18 11:50:09 -0500

    clean up build files

--- build/lib/svae/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Implements a sparse-coding variational autoencoder.
-
-Barello, Gabriel, Adam S. Charles, and Jonathan W. Pillow. "Sparse-Coding Variational
-Auto-Encoders". Preprint, submitted August 23, 2018. http://dx.doi.org/10.1101/399246.
-
-"""
